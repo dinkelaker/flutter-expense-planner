245292078c406f8e33bfebab1eac761928493d4e
Tom Dinkelaker <tom.dinkelaker@gmail.com>
2020-04-17 15:06:14 +0200

    Amount container's decoration

--- lib/main.dart
+++ lib/main.dart
@@ -84,16 +84,24 @@ class _MyHomePageState extends State<MyHomePage> {
               return Row(
                 children: <Widget>[
                   Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 2,
+                      ),
+                    ),
                     child: Card(
-                      child: Text(tx.amount.toString())
+                      child: Text(tx.amount.toString()),
                     ),
                   ),
-                  Column(
-                    children: <Widget>[
-                      Text(tx.title),
-                      Text(tx.date.toString()),
-                    ]
-                  ),
+                  Column(children: <Widget>[
+                    Text(tx.title),
+                    Text(tx.date.toString()),
+                  ]),
                 ],
               );
             }).toList(),
