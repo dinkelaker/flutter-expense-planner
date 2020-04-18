87c7fd9bd958d1d5094712b9c304321c60738414
Tom Dinkelaker <tom.dinkelaker@gmail.com>
2020-04-18 15:51:30 +0200

    Improve scrolling with ListView

--- lib/widget/transaction_list.dart
+++ lib/widget/transaction_list.dart
@@ -12,8 +12,7 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 400,
-      child: SingleChildScrollView(
-        child: Column(
+      child: ListView(
                 children: transactions.map((tx) {
                   return Card(
                     child: Row(
@@ -50,7 +49,6 @@ class TransactionList extends StatelessWidget {
                     ),
                   );
                 }).toList(),
-              ),
       ),
     );
   }
