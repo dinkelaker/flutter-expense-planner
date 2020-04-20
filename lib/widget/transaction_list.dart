bed5f6833c12cec989cb889291222b2e547f6497
Tom Dinkelaker <tom.dinkelaker@gmail.com>
2020-04-21 00:48:08 +0200

    DateTime Picker and Deleting Items

--- lib/widget/transaction_list.dart
+++ lib/widget/transaction_list.dart
@@ -11,101 +11,99 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        height: 400,
-        child: transactions.isNotEmpty
-            ? ListView.builder(
-                itemBuilder: (ctx, index) {
-                  return Card(
-                    elevation: 5,
-                    margin: EdgeInsets.symmetric(
-                      vertical: 8,
-                      horizontal: 5,
-                    ),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        radius: 30,
-                        child: Padding(
-                          padding: EdgeInsets.all(6),
-                          child: FittedBox(
-                            child: Text(
-                                '\$${NumberFormat("##0.00", "en_US").format(transactions[index].amount)}',
-                                style: Theme.of(context).textTheme.title.copyWith(
-                                      fontWeight: FontWeight.bold,
-                                      color: Theme.of(context).primaryColorDark,
-                                    )),
-                          ),
+    return Container(
+      height: 500,
+      child: transactions.isNotEmpty
+          ? ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text(
+                              '\$${NumberFormat("##0.00", "en_US").format(transactions[index].amount)}',
+                              style: Theme.of(context).textTheme.title.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                    color: Theme.of(context).primaryColorDark,
+                                  )),
                         ),
                       ),
-                      title: Text(transactions[index].title,
-                          style: Theme.of(context).textTheme.title),
-                      subtitle: Text(DateFormat('yyyy-MM-dd')
-                          .format(transactions[index].date)),
-                      trailing: IconButton(
-                        icon: Icon(Icons.delete,
-                            color: Theme.of(context).errorColor),
-                        onPressed: () { 
-                          deleteTransaction(transactions[index].id); 
-                        },
-                      ),
                     ),
-                  );
-
-                  //   Card(
-                  //   child: Row(
-                  //     children: <Widget>[
-                  //       Container(
-                  //         margin: EdgeInsets.symmetric(
-                  //           vertical: 10,
-                  //           horizontal: 15,
-                  //         ),
-                  //         decoration: BoxDecoration(
-                  //           border: Border.all(
-                  //             color: Theme.of(context).primaryColor,
-                  //             width: 2,
-                  //           ),
-                  //         ),
-                  //         child: Card(
-                  //           child: Text(
-                  //               '\$${NumberFormat("##0.00", "en_US").format(transactions[index].amount)}',
-                  //               style: Theme.of(context).textTheme.title.copyWith(
-                  //                     fontWeight: FontWeight.bold,
-                  //                     color: Theme.of(context).primaryColorDark,
-                  //                   )),
-                  //         ),
-                  //       ),
-                  //       Column(children: <Widget>[
-                  //         Text(
-                  //           transactions[index].title,
-                  //           style: Theme.of(context).textTheme.title.copyWith(
-                  //                 fontWeight: FontWeight.bold,
-                  //                 fontSize: 16,
-                  //               ),
-                  //         ),
-                  //         Text(DateFormat('yyyy-MM-dd')
-                  //             .format(transactions[index].date)),
-                  //       ]),
-                  //     ],
-                  //   ),
-                  // );
-                },
-                itemCount: transactions.length,
-              )
-            : Column(
-                children: <Widget>[
-                  Text('No transaction added yet!'),
-                  SizedBox(
-                    height: 10,
+                    title: Text(transactions[index].title,
+                        style: Theme.of(context).textTheme.title),
+                    subtitle: Text(DateFormat('yyyy-MM-dd')
+                        .format(transactions[index].date)),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete,
+                          color: Theme.of(context).errorColor),
+                      onPressed: () { 
+                        deleteTransaction(transactions[index].id); 
+                      },
+                    ),
                   ),
-                  Container(
-                    height: 200,
-                    child: Image.asset('assets/images/waiting.png',
-                        fit: BoxFit.cover),
-                  )
-                ],
-              ),
-      ),
+                );
+
+                //   Card(
+                //   child: Row(
+                //     children: <Widget>[
+                //       Container(
+                //         margin: EdgeInsets.symmetric(
+                //           vertical: 10,
+                //           horizontal: 15,
+                //         ),
+                //         decoration: BoxDecoration(
+                //           border: Border.all(
+                //             color: Theme.of(context).primaryColor,
+                //             width: 2,
+                //           ),
+                //         ),
+                //         child: Card(
+                //           child: Text(
+                //               '\$${NumberFormat("##0.00", "en_US").format(transactions[index].amount)}',
+                //               style: Theme.of(context).textTheme.title.copyWith(
+                //                     fontWeight: FontWeight.bold,
+                //                     color: Theme.of(context).primaryColorDark,
+                //                   )),
+                //         ),
+                //       ),
+                //       Column(children: <Widget>[
+                //         Text(
+                //           transactions[index].title,
+                //           style: Theme.of(context).textTheme.title.copyWith(
+                //                 fontWeight: FontWeight.bold,
+                //                 fontSize: 16,
+                //               ),
+                //         ),
+                //         Text(DateFormat('yyyy-MM-dd')
+                //             .format(transactions[index].date)),
+                //       ]),
+                //     ],
+                //   ),
+                // );
+              },
+              itemCount: transactions.length,
+            )
+          : Column(
+              children: <Widget>[
+                Text('No transaction added yet!'),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 200,
+                  child: Image.asset('assets/images/waiting.png',
+                      fit: BoxFit.cover),
+                )
+              ],
+            ),
     );
   }
 }
