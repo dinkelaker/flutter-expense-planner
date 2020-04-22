5c0d7564c453b08cf63e615594bc21c9f1a6e60d
Tom Dinkelaker <tom.dinkelaker@gmail.com>
2020-04-22 08:35:54 +0200

    MediaQuery viewInsets.bottom respects keyboard height (does not yet work, consider ModalBottomSheet)

--- lib/widget/new_transaction.dart
+++ lib/widget/new_transaction.dart
@@ -19,8 +19,7 @@ class _NewTransactionState extends State<NewTransaction> {
     var title = titleController.text;
     var amount = double.parse(amountController.text);
 
-    if (title.isEmpty || amount <= 0 || _selectedDate == null) 
-      return;
+    if (title.isEmpty || amount <= 0 || _selectedDate == null) return;
 
     widget.addTransaction(title, amount, _selectedDate);
 
@@ -46,53 +45,62 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        child: Container(
-            padding: EdgeInsets.all(10),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Title'),
-                    controller: titleController,
-                    onSubmitted: (_) => _submitData(),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Amount'),
-                    controller: amountController,
-                    keyboardType: TextInputType.number,
-                    onSubmitted: (_) => _submitData(),
-                  ),
-                  Container(
-                    height: 70,
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: Text(_selectedDate == null
-                              ? 'No date chosen!'
-                              : 'Date: '+DateFormat('yyyy-MM-dd').format(_selectedDate)),
-                        ),
-                        FlatButton(
-                          child: Text(
-                            'Choose date',
-                            style: Theme.of(context).textTheme.title.copyWith(
-                                  fontWeight: FontWeight.bold,
-                                  color: Theme.of(context).primaryColorDark,
-                                ),
-                          ),
-                          onPressed: () {
-                            _presentDatePicker();
-                          },
-                        )
-                      ],
+    return SingleChildScrollView(
+      child: Card(
+          child: Container(
+              padding: EdgeInsets.only(
+                top: 10,
+                left: 10,
+                right: 10,
+                bottom: MediaQuery.of(context).viewInsets.bottom + 30,
+              ),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                      controller: titleController,
+                      onSubmitted: (_) => _submitData(),
                     ),
-                  ),
-                  RaisedButton(
-                    child: Text('Add Transaction'),
-                    color: Theme.of(context).primaryColor,
-                    textColor: Theme.of(context).textTheme.button.color,
-                    onPressed: _submitData,
-                  )
-                ])));
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Amount'),
+                      controller: amountController,
+                      keyboardType: TextInputType.number,
+                      onSubmitted: (_) => _submitData(),
+                    ),
+                    Container(
+                      height: 70,
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Text(_selectedDate == null
+                                ? 'No date chosen!'
+                                : 'Date: ' +
+                                    DateFormat('yyyy-MM-dd')
+                                        .format(_selectedDate)),
+                          ),
+                          FlatButton(
+                            child: Text(
+                              'Choose date',
+                              style: Theme.of(context).textTheme.title.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                    color: Theme.of(context).primaryColorDark,
+                                  ),
+                            ),
+                            onPressed: () {
+                              _presentDatePicker();
+                            },
+                          )
+                        ],
+                      ),
+                    ),
+                    RaisedButton(
+                      child: Text('Add Transaction'),
+                      color: Theme.of(context).primaryColor,
+                      textColor: Theme.of(context).textTheme.button.color,
+                      onPressed: _submitData,
+                    )
+                  ]))),
+    );
   }
 }
